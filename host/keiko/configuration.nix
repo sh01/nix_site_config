557092bbfb1205b052868b36efa2f39ad8c49c78
Sebastian Hagen <root@keiko.y>
2016-03-28 17:13:13 +0000

    Keiko config: Initial version

---
+++ host/keiko/configuration.nix
@@ -0,0 +1,124 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+
+  ##### Host id stuff
+  networking = {
+    hostName = "keiko.sh.s";
+    hostId = "84d5fcc6";
+    nameservers = [ "10.16.0.1" ];
+    search = [ "sh.s ulwifi.s baughn-sh.s" ];
+    usePredictableInterfaceNames = false;
+  };
+
+  ### Package auth
+  nix.binaryCachePublicKeys = [];
+  nix.binaryCaches = [];
+  
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Select internationalisation properties.
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+    supportedLocales = ["en_US.UTF-8/UTF-8" "en_DK.UTF-8/UTF-8" ];
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Dublin";
+
+  environment.shells = [ "/run/current-system/sw/bin/zsh" ];
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    glibcLocales
+
+    file
+    less
+    most
+    screen
+    tree
+    zsh
+
+    gzip
+    bzip2
+    xz
+ 
+    python
+    python3
+
+    iputils
+    netcat
+    socat
+    tcpdump
+    wget
+
+    btrfsProgs
+
+    manpages
+    man_db
+    posix_man_pages
+  ];
+
+  fileSystems = {
+    "/" = { label = "root"; };
+  };
+
+  fonts.fontconfig.enable = false;
+
+  ### GRUB 2 boot loader.
+  boot.loader.grub.enable = false;
+  boot.loader.grub.version = 2;
+  # Define on which hard drive you want to install Grub.
+  # boot.loader.grub.device = "/dev/sda";
+
+  ### Networking
+  networking.useDHCP = false;
+  networking.dhcpcd.allowInterfaces = [];
+  networking.firewall.allowPing = true;
+  networking.firewall.rejectPackets = true;
+
+  nix.allowedUsers = [ "@nix-users" ];
+
+  ### Nix build config
+  nix.daemonIONiceLevel = 2;
+  nix.daemonNiceLevel = 2;
+  nix.requireSignedBinaryCaches = true;
+
+  ### Services
+  services.openssh.enable = true;
+
+  ### Per-program config
+  programs.ssh.startAgent = false;
+  programs.zsh = {
+    enable = true;
+  };
+
+  ### Accounts
+  # Define a user account.
+  users.extraUsers = {
+    "sh" = {
+      isNormalUser = true;
+      uid = 1000;
+    };
+  };
+
+  # The NixOS release to be compatible with for stateful data such as databases.
+  system.stateVersion = "15.09";
+
+  ### terminal stuff
+  fonts.fontconfig.defaultFonts.serif = [ "DejaVu Sans" ];
+  hardware.opengl.driSupport32Bit = true;
+  hardware.pulseaudio.support32Bit = true;
+}
