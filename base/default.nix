dfef32417dad5d7220c30d4b530bc0f28504e77c
Sebastian Hagen <root@keiko.y>
2016-04-06 00:12:44 +0100

    default.nix: Override priorities of procps and coreutils in an attempt to get a better uptime(1) by default.

--- base/default.nix
+++ base/default.nix
@@ -7,4 +7,15 @@
   environment.shellAliases = {
     ne = "PAGER=cat nix-env";
   };
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    # Put procps below coreutils for uptime(1).
+    # "outPath" entries are a hack around "cannot coerce a set to a string" bug (see https://github.com/NixOS/nixpkgs/issues/7425 )
+    procps = pkgs.procps.overrideDerivation (a: {
+      meta = {priority = 1; outPath="X";};
+    });
+    coreutils = pkgs.coreutils.overrideDerivation (a: {
+      meta = {priority = 2; outPath="X";};
+    });
+  };
 }
