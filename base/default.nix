f7b8c5a83ae569d67559b9c03f2436ee6643c1b7
Sebastian Hagen <root@keiko.y>
2016-04-09 21:33:48 +0100

    default.nix: Fix priority override code to actually work right (and also be simpler).

--- base/default.nix
+++ base/default.nix
@@ -17,12 +17,6 @@
     gnupg = pkgs.gnupg.override { x11Support = false; };
 
     # Put procps below coreutils for uptime(1).
-    # "outPath" entries are a hack around "cannot coerce a set to a string" bug (see https://github.com/NixOS/nixpkgs/issues/7425 )
-    procps = pkgs.procps.overrideDerivation (a: {
-      meta = {priority = 1; outPath="X";};
-    });
-    coreutils = pkgs.coreutils.overrideDerivation (a: {
-      meta = {priority = 2; outPath="X";};
-    });
+    procps = pkgs.lib.hiPrio pkgs.procps;
   };
 }
