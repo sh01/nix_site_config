2c7930a63a12205c9862567164c102ef434d15a1
Sebastian Hagen <sh_c_git@memespace.net>
2016-04-23 05:13:28 +0100

    pkgs/scripts: Add a comment explaining why we have this package.

--- pkgs/scripts/default.nix
+++ pkgs/scripts/default.nix
@@ -1,3 +1,9 @@
+# Stash some scripts that should be invoked from configuration in particular
+# home directories (as after X loging of specific users).
+# By keeping them in a nix package, we can use static reference paths inside
+# the home-dir confs and update the actual scripts along with the rest of the
+# system.
+
 let
   pkgs = import <nixpkgs> {};
 in
