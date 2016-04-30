b85a3edb26ec5b362516b3110268ebabdf4b2ac7
Sebastian Hagen <sh_c_git@memespace.net>
2016-04-30 16:39:43 +0100

    pks/dep/mc0: Add Java dependencies commons-io and commons-compress.

--- pkgs/pkgs/dep/mc0/default.nix
+++ pkgs/pkgs/dep/mc0/default.nix
@@ -5,6 +5,7 @@ with pkgs; derivation {
   LINKNAME = "mc0";
   LDEPS = with pkgs.xorg; [libX11 libXext libXcursor libXrandr libXxf86vm mesa openal];
   BDEPS = [openjdk];
+  JDEPS = [commonsIo commonsCompress];
   builder = ../link_deps;
   src = /var/empty;
 }
