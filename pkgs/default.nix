e97a8d67befd85e6bf5b245b07a4f72915b179a9
Sebastian Hagen <sh_c_git@memespace.net>
2016-04-30 02:55:56 +0100

    pkgs/dep/mc0: Initial version.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7,5 +7,7 @@ let
     start_nix = callPackage ./pkgs/start_nix {};
     SH_scripts = callPackage ./pkgs/scripts {};
     SH_udev_containers = callPackage ./pkgs/udev_containers {};
+
+    SH_dep_mc0 = callPackage ./pkgs/dep/mc0 {};
   };
 in self
---
+++ pkgs/pkgs/dep/mc0/default.nix
@@ -0,0 +1,10 @@
+{pkgs, system, ...}:
+with pkgs; derivation {
+  name = "SH_dep_mc0";
+  inherit system coreutils;
+  LINKNAME = "mc0";
+  LDEPS = with pkgs.xorg; [libX11 libXext libXcursor libXrandr libXxf86vm mesa openal];
+  BDEPS = [openjdk];
+  builder = ../link_deps;
+  src = /var/empty;
+}
