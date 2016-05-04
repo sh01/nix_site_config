1d5a31bab6dcbceb19f8d9c6041aa2b953cfbb06
Sebastian Hagen <sh_c_git@memespace.net>
2016-05-04 02:34:42 +0100

    host/yalda: Add /etc/X11/xorg.conf.d/999-monitor.conf for PG278Q mode spec.

--- host/yalda/hardware-configuration.nix
+++ host/yalda/hardware-configuration.nix
@@ -3,6 +3,12 @@
 {
   imports = [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix> ];
 
+  # PG278Q+intel IGP DP auto-config does not work correctly.
+  # Making this work using services.xserver.*Section is a pain. Much easier to just drop in a file.
+  environment.etc."X11/xorg.conf.d/999-monitor.conf" = {
+     text = (builtins.readFile ./999-monitor.conf);
+  };
+  
   boot.extraModulePackages = [ ];
   swapDevices = [];
   nix.maxJobs = 8;
