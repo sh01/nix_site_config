b9095f46d52abbb2d8e14864975d93b8e07d3507
Sebastian Hagen <sh_c_git@memespace.net>
2016-05-11 13:59:19 +0100

    rune: hardware.enableAllFirmware = true

--- host/rune/hardware-configuration.nix
+++ host/rune/hardware-configuration.nix
@@ -3,6 +3,8 @@
 {
   imports = [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix> ];
 
+  hardware.enableAllFirmware = true;
+
   boot.extraModulePackages = [ ];
   swapDevices = [];
   nix.maxJobs = 4;
