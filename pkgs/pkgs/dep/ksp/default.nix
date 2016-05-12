e863d8356bf6d009ee6b51f23157c9690fe17af4
Sebastian Hagen <sh_c_git@memespace.net>
2016-05-12 02:21:18 +0100

    dep/KSP: Add libtxc_dxtn.

--- pkgs/pkgs/dep/ksp/default.nix
+++ pkgs/pkgs/dep/ksp/default.nix
@@ -4,7 +4,7 @@ with pkgs; (callPackage ../base.nix {
   LDEPS = with pkgs.xorg; [
     stdenv.cc.cc
     # Graphics stuff
-    libX11 libXcursor libXrandr mesa
+    libX11 libXcursor libXrandr mesa libtxc_dxtn
     # Audio stuff. KSP supports output through pulseaudio directly.
     libpulseaudio
   ];
