ff518c416eeebc628645118ae75f03f0451602ce
Sebastian Hagen <sh_c_git@memespace.net>
2016-05-13 01:53:35 +0100

    base/alsa2pulse: Also link 32-bit libs in config, to allow sound output for such binaries.

--- base/alsa2pulse.nix
+++ base/alsa2pulse.nix
@@ -1,12 +1,14 @@
-{pkgs, ...}:
+{pkgs, pkgs_i686, ...}:
 
 # /etc/asound.conf for forwarding to pulseaudio.
 # This is copied from modules.config.pulseaudio; we can't get that one without the full pulse package, which we don't need here.
 let
   inherit (pkgs) alsaPlugins;
+  alsaPlugins32 = pkgs_i686.alsaPlugins;
   alsaConf = pkgs.writeText "asound.conf" ''
     pcm_type.pulse {
       lib ${alsaPlugins}/lib/alsa-lib/libasound_module_pcm_pulse.so
+      lib ${alsaPlugins32}/lib/alsa-lib/libasound_module_pcm_pulse.so
     }
     pcm.!default {
       type pulse
@@ -14,6 +16,7 @@ let
     }
     ctl_type.pulse {
       lib ${alsaPlugins}/lib/alsa-lib/libasound_module_ctl_pulse.so
+      lib ${alsaPlugins32}/lib/alsa-lib/libasound_module_ctl_pulse.so
     }
     ctl.!default {
       type pulse
