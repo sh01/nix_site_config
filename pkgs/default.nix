b08491765509c2415c016f133a24785a7cf1bc27
Sebastian Hagen <sh_c_git@memespace.net>
2016-10-08 06:49:35 +0100

    SH_dep_ggame*: Adjust names on ggame* to match mapping key. Add some packages for X3 compatibility.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -19,7 +19,7 @@ let
     SH_dep_WL2 = callPackage ./pkgs/dep/wl2 {};
     SH_dep_Stellaris = callPackage ./pkgs/dep/stellaris {};
     SH_dep_starbound = callPackage ./pkgs/dep/starbound {};
-    SH_dep_ggame = callPackage ./pkgs/dep/ggame {};
-    SH_dep_ggame32 = callPackage ./pkgs/dep/ggame {pkgs = arch32.pkgs;};
+    SH_dep_ggame = callPackage ./pkgs/dep/ggame {name = "ggame";};
+    SH_dep_ggame32 = callPackage ./pkgs/dep/ggame {pkgs = arch32.pkgs; name = "ggame32";};
   };
 in pkgs
