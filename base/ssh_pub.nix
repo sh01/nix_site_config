ba840f0d06939869cb50fb26269e852c4bc01cdd
Sebastian Hagen <sh_c_git@memespace.net>
2017-04-06 01:12:57 +0100

    ssh keys: Add ika hostkey.

--- base/ssh_pub.nix
+++ base/ssh_pub.nix
@@ -26,6 +26,7 @@ in rec {
   hk_uiharu = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKLN1lfEixR0e6XLycRC2FWIVmPtcLcdMj3SxnNWj275";
   hk_yalda = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP5W68IOU9/E5wcKML27gd5Z3JpmX5nHAeNX8iiNBG1g";
   hk_rune = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILLzM89Ec/M3/jod75DuPVmeZimXEHiSjM+NpKUnsl/p";
+  hk_ika = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOGY7dn1FVGVibtkYwIE+g87mTRG1XE7C8jhAe3mARTv";
 
   hk_bw0 = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDJtyFexV2+utU0Y0EYuJoxgfNKUcOqQ7yCx0QgKhEfbdmBB2U/usZ0gIoTT0pxoqbPOuqk1YYza7BwxM6AJs7hGcuMmRzsqSU9eG9Ow8JT7NyhdLUKes37U+6EA1vea2JmNsvmGvzsmRVB3/tDGpsoSgJhWsKK2Mboc1n6g5UAC+8GHDn329N6nQ7u/wucwC6vFEZa/T2Fppu79eKgjxpyDRO1iWHiEE8pO8mbFWQfrvoKcoyIWbjdh/6s9sARrZ1w15j90OFDOpPMKxIIOff5CIiiwQERdmRL/QNtZOkOCoEUUgU2byKNASoieC8w0voh6OUOtgoecjWsLTiNXJ5Z";
   
@@ -36,6 +37,7 @@ in rec {
     { hostNames = HN "uiharu"; publicKey = hk_uiharu;}
     { hostNames = HN "yalda"; publicKey = hk_yalda;}
     { hostNames = HN "rune"; publicKey = hk_rune;}
+    { hostNames = [ "ika.r.sh.s" "138.68.246.52"]; publicKey = hk_ika;}
     { hostNames = ["bw0" "bw0.ulwired-ctl.s."]; publicKey = hk_bw0;}
   ];
 }
