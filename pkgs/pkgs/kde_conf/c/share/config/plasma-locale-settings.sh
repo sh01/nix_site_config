3aaca517bc21c5153d529bdf7afa55515f9aac66
Sebastian Hagen <sh_c_git@memespace.net>
2017-07-02 19:48:47 +0100

    KDE setup: Update configs to deal with Plasma 5.

---
+++ pkgs/pkgs/kde_conf/c/share/config/plasma-locale-settings.sh
@@ -0,0 +1,10 @@
+# Generated script, do not edit
+# Exports language-format specific env vars from startkde.
+# This script has been generated from kcmshell5 formats.
+# It will automatically be overwritten from there.
+export LANG=en_US.UTF-8
+export LC_NUMERIC=C
+export LC_TIME=iu_CA.UTF-8
+export LC_MONETARY=en_IE.UTF-8
+export LC_MEASUREMENT=en_IE.UTF-8
+export LC_COLLATE=en_US.UTF-8
