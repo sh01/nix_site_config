7e2161646f2f9d06ca448212e195c1959a289946
Sebastian Hagen <sh_c_git@memespace.net>
2018-01-15 06:47:00 +0000

    yalda, rune: Set hardware.cpu.intel.updateMicrocode = true for Spectre-mitigation patches.

--- host/rune/hardware-configuration.nix
+++ host/rune/hardware-configuration.nix
@@ -4,6 +4,7 @@
   imports = [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix> ];
 
   hardware.enableAllFirmware = true;
+  hardware.cpu.intel.updateMicrocode = true;
 
   boot.extraModulePackages = [ ];
   swapDevices = [];
