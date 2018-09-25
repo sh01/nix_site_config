f1b81e686a3875f11d43a0d7bee60fd6a9ec7286
Sebastian Hagen <sh_c_git@memespace.net>
2018-09-25 18:00:58 +0100

    default_kernel: 4.14.12 -> 4.14.71

--- base/default_kernel.nix
+++ base/default_kernel.nix
@@ -1,10 +1,10 @@
 {pkgs, fetchurl, stdenv, perl, buildLinux, config, hostPlatform, ...}: let
   vars = import vars.nix;
 in (import <nixpkgs/pkgs/os-specific/linux/kernel/generic.nix> (rec {
-  version = "4.14.12";
+  version = "4.14.71";
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1bsn73h3ilf7msyiqm5ny2zdj30b9r7k9sc8i03w3iggh3agf236";
+    sha256 = "12blj3zzvzs2nz8rp1g5ykibfvm355442c31l4ijr4mxphylg93n";
   };
 
   inherit stdenv perl buildLinux hostPlatform;
