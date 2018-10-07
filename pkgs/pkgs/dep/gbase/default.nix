086a9df801e3332b010e0df0cdf069f2c8d678cc
Sebastian Hagen <sh_c_git@memespace.net>
2018-10-07 21:17:23 +0100

    Rework dep packages: Put everything under sw/local/ and add that path to environment.pathsToLink for all builds. This finally makes ggame dirs work right for containers!

--- pkgs/pkgs/dep/gbase/default.nix
+++ pkgs/pkgs/dep/gbase/default.nix
@@ -4,12 +4,8 @@ with pkgs; stdenv.mkDerivation {
   name = "SH_dep_gbase";
   phases = ["installPhase"];
   installPhase = ''
-    mkdir -p $out/share/gbase; cd $out/share/gbase
-    mkdir ggame
-    ln -s ${SH_dep_ggame}/share/lapps/ggame/ ggame/64
-    ln -s ${SH_dep_ggame32}/share/lapps/ggame32/ ggame/32
-    mkdir ld
-    ln -s ${glibc}/lib/ld-linux*so* ld/64
-    ln -s ${arch32.glibc}/lib/ld-linux*so* ld/32
+    mkdir -p $out/local/ld; cd $out/local/ld
+    ln -s ${glibc}/lib/ld-linux*so* 64
+    ln -s ${arch32.glibc}/lib/ld-linux*so* 32
   '';
 }
