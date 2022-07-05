36492e04cd33a949e35ba0dcae5f4ef260e18fc6
Sebastian Hagen <sh_c_git@memespace.net>
2022-07-04 19:20:23 -0700

    yalda: Update ipv4 address.

--- nix/host/yalda/configuration.nix
+++ nix/host/yalda/configuration.nix
@@ -45,13 +45,13 @@ in rec {
     interfaces = {
       "eth_lan" = {
         ipv4.addresses = [{
-          address = "10.16.0.65";
+          address = "10.17.1.10";
           prefixLength = 24;
         }];
         ipv4.routes = [{
           address = "0.0.0.0";
           prefixLength = 0;
-          via = "10.16.0.1";
+          via = "10.17.1.1";
         }];
         ipv6.addresses = [
           { address = "2001:470:7af3:1:1:0:2:1"; prefixLength = 80;}
@@ -59,7 +59,7 @@ in rec {
         ];
         ipv6.routes = [
           { address = "fd9d:1852:3555::"; prefixLength = 48; via = "fd9d:1852:3555:200::1";}
-          { address = "::"; prefixLength = 0; via = "2001:470:7af3:1:1::1";}
+          #{ address = "::"; prefixLength = 0; via = "2001:470:7af3:1:1::1";}
         ];
       };
     };
