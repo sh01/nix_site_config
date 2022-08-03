0e1821c69de51b6e3fb32a4d6619555bf0ce78ef
Sebastian Hagen <sh_c_git@memespace.net>
2022-08-03 00:49:33 -0700

    Followup: Add kernel_conf.nix for host/nova.

---
+++ nix/host/nova/kernel_conf.nix
@@ -0,0 +1,13 @@
+{lib, ...}:
+let
+  inherit (lib) mkForce;
+  vars = (import ../../base/vars.nix {inherit lib;});
+  ko = vars.kernelOpts;
+in with ko; with (import <nixpkgs/lib/kernel.nix> {lib = null;}); base // netStd // termHwStd // termVideo // hwAudio // blkStd // {
+DRM_I915 = mkForce yes;
+SND_SOC_SOF_HDA = yes;
+SND_SOC_SOF_HDA_AUDIO_CODEC = yes;
+SND_SOC_SOF_APOLLOLAKE = yes;
+SND_SOC_SOF_INTEL_APL = yes;
+HDMI_LPE_AUDIO = yes;
+}
