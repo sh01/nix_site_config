97dd9852080180cecbba5ff7b797dd368502b338
Sebastian Hagen <sh_c_git@memespace.net>
2022-10-15 21:22:19 -0500

    game_pad: Make udev recognize xboxdrv devices.

--- nix/base/term/game_pads.nix
+++ nix/base/term/game_pads.nix
@@ -3,6 +3,7 @@
   services.udev.extraRules = ''
 SUBSYSTEM=="usb", ACTION=="add", ATTR{product}=="Controller", GROUP="game_pad"
 SUBSYSTEM=="input", ATTRS{product}=="Controller", GROUP="input_game"
+SUBSYSTEM=="input", ATTRS{name}=="Xbox Gamepad (userspace driver)", GROUP="input_game"
 KERNEL=="uinput", GROUP="game_pad", MODE="660"
 '';
 }
