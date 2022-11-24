30079200aa1a67bffe2e0794a2c3b460769aaf62
root@jibril <root@jibril>
2022-11-23 19:39:20 -0600

    jibril: Grant access to rtanen.

--- nix/host/jibril/default.nix
+++ nix/host/jibril/default.nix
@@ -100,7 +100,7 @@ in rec {
 
   ### User / Group config
   # Define paired user/group accounts.
-  users = slib.mkUserGroups (with vars.userSpecs {}; default ++ [sophia]);
+  users = slib.mkUserGroups (with vars.userSpecs {}; default ++ [sophia rtanen]);
 
   # The NixOS release to be compatible with for stateful data such as databases.
   system.stateVersion = "21.11";
