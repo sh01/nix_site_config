8bf9e3c55e20915238de1720048ae77901d3ddf0
Sebastian Hagen <sh_c_git@memespace.net>
2023-12-16 23:45:59 -0600

    pkgs/meta: Fix to pull in dependencies correctly for nix, avoiding them being discarded due to lack of references in output files.

--- nix/pkgs/pkgs/meta/default.nix
+++ nix/pkgs/pkgs/meta/default.nix
@@ -1,11 +1,18 @@
-{pkgs, system}:
-let P = name: d: derivation {
-  name = "SH_M_" + name;
-  system = system;
-  coreutils = pkgs.coreutils;
-  builder = ./store_pue;
-  propagatedUserEnvPkgs = d;
-}; in with pkgs; rec {
+{pkgs, lib, stdenv, system}:
+let P = bname: d: stdenv.mkDerivation (rec {
+    name = "SH_M_" + bname;
+    deps = lib.strings.concatStringsSep " " d;
+    buildInputs = [pkgs.coreutils];
+      unpackPhase = "#";
+      buildPhase = ''
+        RD="$out/local/_reflinks"
+        DD="$out/nix-support"
+        mkdir -p "$RD" "$DD"
+        echo "${deps}" > "$RD/$name"
+        echo "${deps}" > "$DD/propagated-user-env-packages"
+'';
+    });
+in with pkgs; rec {
   emacs_packages = P "emacs_packages" (pkgs.callPackage ../emacs {}).emacsPackages;
 
   ### Base utilities and libraries
