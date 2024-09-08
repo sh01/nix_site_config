60c4098650f42f16fb17425b1bb864e15a77a6ef
Sebastian Hagen <sh_c_git@memespace.net>
2024-09-07 23:42:12 -0500

    Followup fix.

--- nix/base/channel.nix
+++ nix/base/channel.nix
@@ -2,20 +2,14 @@
 let
   nixVer = "24.05";
   npkg = name: {
-    "$name" = {
+    "${name}" = {
       exact = false;
-      from = { id = name; type = "indirect";};
       to = { type = "git"; url = "file:/root/git/nixpkgs?ref=l${nixVer}";};
     };
   };
 in {
   # Make sure we don't accidentally retrieve to-be-executed code through insufficiently authenticated channels
   system.defaultChannel = "file:///var/local/nix/c0";
-  nixpkgs = {
-    flake = {
-      setFlakeRegistry = true;
-    };
-  };
   nix = {
     channel.enable = false;
     registry = (npkg "nixpkgs") // (npkg "local_nixpkgs");
