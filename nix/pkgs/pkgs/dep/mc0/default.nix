65865b2ee5726aae6ba1d33f6ec0311e04b9143b
Sebastian Hagen <sh_c_git@memespace.net>
2024-09-11 01:33:21 -0500

    pkgs: Drop obsolete dep pkgs.

--- nix/pkgs/pkgs/dep/mc0/default.nix
+++
@@ -1,7 +0,0 @@
-{pkgs, system, callPackage, ...}:
-with pkgs; (callPackage ../base.nix {
-  name = "mc0";
-  LDEPS = with pkgs.xorg; [libX11 libXext libXcursor libXrandr libXxf86vm libGL_driver openal libpulseaudio libvorbis];
-  BDEPS = [openjdk];
-  JDEPS = [commonsIo commonsCompress];
-})
