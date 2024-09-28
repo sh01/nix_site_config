3297b99cbff5ea75d6999d7d084957f28c13e95f
Sebastian Hagen <sh_c_git@memespace.net>
2024-09-28 04:00:46 -0500

    lib/: Simplify nix hostId calculation. We expect this field to be unimportant for our setups, and keeping stability is a lower priority than simplicity.

--- nix/lib/l.nix
+++ nix/lib/l.nix
@@ -1,6 +1,9 @@
 {hostc, hostname}: {lib, config, pkgs, ...}:
 let
   inherit (builtins) elemAt;
+  inherit (lib.strings) fixedWidthString;
+  inherit (lib.trivial) toHexString;
+
   callWith = autoargs: fn: args:
     let
       f = if lib.isFunction fn then fn else import fn;
@@ -11,26 +14,32 @@ let
     inherit lib config pkgs call l;
   };
 
-  _hostsData = {
-    "uiharu" = ["84d5fccd" 6 "wi"];
-  };
-
-  _hostData = _hostsData."${hostname}";
-  _hostIdx = elemAt _hostData 1;
   _sites = call ../base/sites.nix {};
 
+  # By-host constant configuration. These are split out here over being kept in
+  # per-host config files to make sanity verification and mass edits easier.
+  _hostsData = {
+    "uiharu" = [6 "wi"];
+  };
+
+  # Host-specific variables
+  _hostData = _hostsData."${hostname}";
+  _hostIdx = elemAt _hostData 0;
+  _nixHostId = fixedWidthString 8 "0" (toHexString (65536 + _hostIdx));
+
+  # Will be passed as argument "l" to host configs and anything below called via l.call.
   l = rec {
     inherit hostname;
     lib = call ../lib {};
     vars = call ../base/vars.nix {};
     defaultConf = call (import ../base);
     
-    site = _sites."${elemAt _hostData 2}";
+    site = _sites."${elemAt _hostData 1}";
     siteConf = site.config;
     
     netHostInfo = {
       hostName = hostname;
-      hostId = elemAt _hostData 0;
+      hostId = _nixHostId;
     };
 
     ifaceDmz = site.mkIface _hostIdx;
