665d23698d26260ee71c861c62a47178d456b0cc
Sebastian Hagen <sh_c_git@memespace.net>
2024-09-29 04:49:28 -0500

    Followup: -dead code

--- nix/lib/host_config.nix
+++ nix/lib/host_config.nix
@@ -1,7 +1,6 @@
 {system, lib, pkgs, ...}@args:
 let
   hostname = builtins.getEnv "NIX_HOST";
-  #host = (import ../host)."${hostname}";
   hostc = ../host + ("/" + hostname);
   conf = (import ./l.nix {inherit hostname hostc;} args).call hostc {};
 in conf
