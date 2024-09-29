6f6a0c5ff10423049731b71d858dd38c08c29de5
Sebastian Hagen <sh_c_git@memespace.net>
2024-09-29 03:24:13 -0500

    host/keiko: Refactor-simplify config.

--- nix/host/keiko/default.nix
+++ nix/host/keiko/default.nix
@@ -4,9 +4,6 @@
 let
   inherit (pkgs) callPackage;
   inherit (lib) mkForce;
-  ssh_pub = import ../../base/ssh_pub.nix;
-  slib = callPackage ../../lib {};
-  vars = callPackage ../../base/vars.nix {};
   dns = (import ../../base/dns.nix) {};
 in {
   imports = with l.conf; [
@@ -20,11 +17,8 @@ in {
 
   #environment.etc."nix/nix.conf".source = mkForce (builtins.readFile ../../base/nix.conf);
   ##### Host id stuff
-  networking = {
-    hostName = "keiko";
-    hostId = "84d5fcc6";
-    usePredictableInterfaceNames = false;
-    iproute2 = vars.iproute2;
+  networking = l.netHostInfo // {
+    iproute2 = l.vars.iproute2;
     interfaces = {
       "eth_lan" = {
         ipv4.addresses = [
@@ -45,10 +39,6 @@ in {
       };
     };
     firewall.enable = false;
-    useDHCP = false;
-    dhcpcd.allowInterfaces = [];
-    useNetworkd = false;
-
     resolvconf.extraConfig = "resolv_conf=/etc/__resolvconf.out";
 
     nftables = {
@@ -66,7 +56,7 @@ in {
   '';
 
   ### System profile packages
-  environment.systemPackages = with pkgs; with (pkgs.callPackage ../../pkgs/pkgs/meta {}); [
+  environment.systemPackages = with pkgs; with (l.call ../../pkgs/pkgs/meta {}); [
     base
     cliStd
     nixBld
@@ -146,7 +136,7 @@ test -e /dev/mapper/a7-main
   # boot.loader.initScript.enable = true;
   hardware.cpu.intel.updateMicrocode = true;
   boot = {
-    kernelPackages = pkgs.linuxPackagesFor (pkgs.callPackage ../../base/default_kernel.nix {structuredExtraConfig = (import ./kernel_conf.nix {inherit lib;});});
+    kernelPackages = pkgs.linuxPackagesFor (callPackage ../../base/default_kernel.nix {structuredExtraConfig = (l.call ./kernel_conf.nix {});});
     blacklistedKernelModules = ["snd" "rfkill" "fjes" "8250_fintek" "eeepc_wmi" "autofs4" "psmouse"] ++ ["firewire_ohci" "firewire_core" "firewire_sbp2"];
     initrd = {
       luks.devices = {
@@ -174,20 +164,16 @@ test -e /dev/mapper/a7-main
   ### Networking
 
   ### Services
-  services.openssh.enable = true;
   services.openssh.moduliFile = ./sshd_moduli;
   services.uptimed.enable = true;
 
   ### User / Group config
   # Define paired user/group accounts.
-  users = slib.mkUserGroups (with vars.userSpecs {}; default ++ [
+  users = l.lib.mkUserGroups (with l.vars.userSpecs {}; default ++ [
     sh cc es_github mail-sh
     sh_yalda
   ]);
 
   # The NixOS release to be compatible with for stateful data such as databases.
   system.stateVersion = "16.09";
-
-  hardware.opengl.driSupport32Bit = true;
-  hardware.pulseaudio.support32Bit = true;
 }
