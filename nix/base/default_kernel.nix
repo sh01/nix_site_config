b234c2a3ce3380bb2cc636c73e6f558518bc08bd
Sebastian Hagen <sh_c_git@memespace.net>
2024-10-06 20:14:06 -0500

    default_kernel: Refactor.

--- nix/base/default_kernel.nix
+++ nix/base/default_kernel.nix
@@ -1,21 +1,21 @@
 {pkgs, structuredExtraConfig ? {}, ...}: let
   inherit (pkgs) callPackage buildPackages ncurses fetchurl stdenv perl buildLinux hostPlatform;
-in (buildLinux rec {
-  version = "6.1.60";
-  src = fetchurl {
-    url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-    sha256 = "58520e7ae5a6af254ddf7ddbfc42e4373b0d36c67d467f6e35a3bd1672f5fb0a";
+
+  features = {
+    iwlwifi = false;
+    efiBootStub = true;
+    needsCifsUtils = false;
+    netfilterRPFilter = true;
   };
+  
+  cKernel = (buildLinux (rec {
+    version = "6.1.60";
+    src = fetchurl {
+      url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
+      sha256 = "58520e7ae5a6af254ddf7ddbfc42e4373b0d36c67d467f6e35a3bd1672f5fb0a";
+    };
 
-  inherit buildPackages callPackage ncurses stdenv perl buildLinux hostPlatform structuredExtraConfig;
-  #kernelPatches = [ pkgs.kernelPatches.modinst_arg_list_too_long ];
-  kernelPatches = [];
-
-  features.iwlwifi = true;
-  features.efiBootStub = true;
-  features.needsCifsUtils = false;
-  features.netfilterRPFilter = true;
-}).overrideDerivation (oldAttrs: {
-  # Monkey-patch compatibility for newer kernel versions
-  # nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [cpio];
-})
+    inherit buildPackages callPackage ncurses stdenv perl buildLinux hostPlatform structuredExtraConfig;
+    kernelPatches = [];
+  } // features));
+in cKernel
