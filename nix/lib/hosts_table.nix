730b089eb444d600b949eb0c17c5be144c0812f1
Sebastian Hagen <sh_c_git@memespace.net>
2024-10-07 00:39:08 -0500

    wireguard: Switch default to off. Add peers to liel, uiharu, bw0.

--- nix/lib/hosts_table.nix
+++ nix/lib/hosts_table.nix
@@ -7,7 +7,7 @@
   HN = s: [s "${s}.sh.s." "${s}.sh.s" "${s}.vpn.sh.s." "${s}.vpn.sh.s"];
   hostRecord = {
     hn, idx, hk_ssh ? "ssh-ed25519 _NOKEY", hk_wg ? null, site_name ? null, names ? HN hn, addr_global ? null,
-    wg_want_peer ? (x: true)
+    wg_want_peer ? (x: false)
   }: {
     "${hn}" = rec {
       inherit idx names;
@@ -34,6 +34,7 @@
   aAddrG = addr: {addr_global = addr;};
 
   rWgPeers = l: {wg_want_peer = p: (lists.findFirstIndex (x: x == p.name) null l) != null;};
+  rWgPeersAll = {wg_want_peer = p: true;};
   
   s = {
     wi = aSite "wi";
@@ -49,10 +50,10 @@ in (foldl' (l: row: let
   hrec = hostRecord args;
 in (l // hrec)) {} [
   # wi
-  [s.wi (aB 1 "bw0") (aSSHed "AAAAC3NzaC1lZDI1NTE5AAAAICM1FxTZk1oV5gEz70x9q6ahbeScWgg2lTKXStAgn3XM") (aNames ["bw0" "bw0.ulwired-ctl.s."])]
+  [s.wi (aB 1 "bw0") (aSSHed "AAAAC3NzaC1lZDI1NTE5AAAAICM1FxTZk1oV5gEz70x9q6ahbeScWgg2lTKXStAgn3XM") (aNames ["bw0" "bw0.ulwired-ctl.s."]) rWgPeersAll]
   [s.wi (aB null "allison") (aSSHed "AAAAC3NzaC1lZDI1NTE5AAAAIGE+YvDLKwJ9SEm4NgYOELl0TWomv3fGSA7fwLjDWI9I")]
-  [s.wi (aB 6 "liel") (aSSHed "AAAAC3NzaC1lZDI1NTE5AAAAICM1FxTZk1oV5gEz70x9q6ahbeScWgg2lTKXStAgn3XM") (wgk "4dU8QG6UrGkcRREoiO/LCc2EixlaUMKbbdsjGIrOdg0=")]
-  [s.wi (aB 10 "uiharu") (aSSHed "AAAAC3NzaC1lZDI1NTE5AAAAIC6aMRME0BCal6Fn5HhM3HDeFmOf5Ya9jCi2v4vFB5fX") (wgk "/4OTj3kJk0GyAKXncjuNGjJmIfZhClf2fRiuWN3IUEM=")]
+  [s.wi (aB 6 "liel") (aSSHed "AAAAC3NzaC1lZDI1NTE5AAAAICM1FxTZk1oV5gEz70x9q6ahbeScWgg2lTKXStAgn3XM") (wgk "4dU8QG6UrGkcRREoiO/LCc2EixlaUMKbbdsjGIrOdg0=") (rWgPeers ["bw0" "ika"])]
+  [s.wi (aB 10 "uiharu") (aSSHed "AAAAC3NzaC1lZDI1NTE5AAAAIC6aMRME0BCal6Fn5HhM3HDeFmOf5Ya9jCi2v4vFB5fX") (wgk "/4OTj3kJk0GyAKXncjuNGjJmIfZhClf2fRiuWN3IUEM=") (rWgPeers ["bw0"])]
   [s.wi (aB 11 "keiko") (aSSHed "AAAAC3NzaC1lZDI1NTE5AAAAIBVD38g8sHkB1uacAGul7RI/0C4tAmHZOfxAr4ignuUM")]
   [s.wi (aB null "nova")]
   [s.wi (aB null "jibril")]
